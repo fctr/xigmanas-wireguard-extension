5947fece9e24b76e41285af551621ed7d693b696
FCTR <github@fctr.net>
2023-02-08 23:26:53 -0500

    Updated PHP

--- update.cmd
+++ update.cmd
@@ -1,4 +1,5 @@
 git add .
 git commit -m "Updated PHP"
+git pull origin master
 git push origin master
 scp /cygdrive/c/Users/Shaun.FCTR/Downloads/Apps/PortableGit/xigmanas-wireguard-extension/gui/wireguard-gui.php root@192.168.39.3:/mnt/nvme/extensions/wireguard/gui/wireguard-gui.php
